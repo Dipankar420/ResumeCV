93c02956a8a74d482bad090c6aafc48630521776
Dipankar Garai <dipugarai915@gmail.com>
2021-05-25 12:09:30 +0530

    counter section added

--- js/app.js
+++ js/app.js
@@ -11,4 +11,40 @@ $(document).ready(function () {
     bar.style.width = percentage + "%";
     console.log(percentage);
   });
+
+  //counter
+  // const counters = document.querySelectorAll(".counter");
+  // function runCounter() {
+  //   counters.forEach((counter) => {
+  //     counter.innerText = 0;
+
+  //     let target = +counter.dataset.count;
+  //     let step = target / 100;
+  //     // console.log(target);
+  //     let countIt = function () {
+  //       let displayedCount = +counter.innerText;
+  //       if (displayedCount < target) {
+  //         counter.innerText = Math.ceil(displayedCount + step);
+  //         setTimeout(countIt, 1);
+  //       } else {
+  //         counter.innerText = target;
+  //       }
+  //     };
+  //     countIt();
+  //   });
+  // }
+  // // runCounter();
+  // let options = {
+  //   rootMargin: "0px 0px -200px 0px",
+  // };
+  // let done = 0;
+
+  // let counterSection = document.querySelectorAll(".counter_section");
+  // const sectionObserver = new IntersectionObserver(function (entries) {
+  //   if (entries[0].isIntersecting && done !== 1) {
+  //     done = 1;
+  //     runCounter();
+  //   }
+  // }, options);
+  // sectionObserver.observe(counterSection);
 });
